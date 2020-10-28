f3905b68a45e4c4d04add2e8272a65a997ce1c47
Fofanius <vova19007@gmail.com>
2020-10-28 22:19:41 +0200

    Правки меню создания файла

--- TextEditor/Assets/TextEditor/Editor/MenuUtility.cs
+++ TextEditor/Assets/TextEditor/Editor/MenuUtility.cs
@@ -7,16 +7,22 @@ namespace TextEditor.Editor
 {
     public class MenuUtility : UnityEditor.Editor
     {
-        [MenuItem("Assets/Create/Text/TEXT", false, 128)]
+        [MenuItem("Assets/Create/Text Asset/Plain text", false, 128)]
         public static void CreateTextFile()
         {
             CreateTextAssetPopup.ShowPopup(PathUtility.GetPathBySelectionSafe(), "New text", "txt");
         }
 
-        [MenuItem("Assets/Create/Text/README", false, 128)]
+        [MenuItem("Assets/Create/Text Asset/Markdown", false, 128)]
         public static void CreateMarkdownFile()
         {
             CreateTextAssetPopup.ShowPopup(PathUtility.GetPathBySelectionSafe(), "README", "md");
+        }
+        
+        [MenuItem("Assets/Create/Text Asset/Json", false, 128)]
+        public static void CreateJsonFile()
+        {
+            CreateTextAssetPopup.ShowPopup(PathUtility.GetPathBySelectionSafe(), "New json", "json");
         }
 
         [OnOpenAsset(50)]
